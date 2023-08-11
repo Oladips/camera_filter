7dcc3dfff87a83b6fa8801b630abe61c125db8ef
Adedoyin Oladipupo <abiodundipupo@gmail.com>
2023-08-11 16:58:30 +0100

    push

--- lib/camera_filters.dart
+++ lib/camera_filters.dart
@@ -385,7 +385,8 @@ class _CameraScreenState extends State<CameraScreenPlugin> with TickerProviderSt
       cameras[0],
       ResolutionPreset.high,
       enableAudio: true,
-      imageFormatGroup: Platform.isAndroid ? ImageFormatGroup.jpeg : ImageFormatGroup.bgra8888,
+      imageFormatGroup: ImageFormatGroup.yuv420,
+      // imageFormatGroup: Platform.isAndroid ? ImageFormatGroup.jpeg : ImageFormatGroup.yuv420,
     );
     _currentFlashMode = controller?.value.flashMode;
     await controller?.initialize().then((value) async {
